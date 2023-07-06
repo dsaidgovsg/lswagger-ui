d4901dd322b47ff46b15f5de6af82f567d60cd3b
Alvin <alvin@dsaid.gov.sg>
2023-07-06 10:14:48 +0800

    fix for review, add text

--- src/plugins/saml-auth/components/DefinitionSelect.jsx
+++ src/plugins/saml-auth/components/DefinitionSelect.jsx
@@ -19,26 +19,31 @@ export default class DefinitionSelect extends React.Component {
   render() {
     const { definitions, getComponent } = this.props
     const Button = getComponent("Button")
-
-    if (!definitions) return null
+    const samlAuthDefinition = definitions.find(definition => definition.first().get("saml"))
+    const otpAuthDefinition = definitions.find(definition => definition.first().get("otp"))
+    const [samlAuthId] = samlAuthDefinition.keys()
+    const [otpAuthId] = otpAuthDefinition.keys()
 
     return (
       <div className="definition-select">
-        {definitions.map((definition) => {
-          const [authId] = definition.keys()
-          const schema = definition.first()
-          return (
-            <Button
-              className="btn definition-option-btn"
-              role="button"
-              key={authId}
-              onClick={this.handleSelect(authId)}
-            >
-              <strong>{schema.get("title") || authId + ".title"}</strong>
-              <p>{schema.get("description") || authId + ".description"}</p>
-            </Button>
-          )
-        })}
+        <Button
+          className="btn definition-option-btn"
+          role="button"
+          key={samlAuthId}
+          onClick={this.handleSelect(samlAuthId)}
+        >
+          <strong>Singapore Government Officers</strong>
+          <p>You have a Government issued email address. Login in using your WOG credentials</p>
+        </Button>
+        <Button
+          className="btn definition-option-btn"
+          role="button"
+          key={otpAuthId}
+          onClick={this.handleSelect(otpAuthId)}
+        >
+          <strong>Others</strong>
+          <p>No Government issued email address. Log in using your email and OTP</p>
+        </Button>
       </div>
     )
   }
